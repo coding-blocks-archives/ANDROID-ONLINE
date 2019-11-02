98b5efbbaac0f9e2c8358c1306176463ad291dc0
Pulkit <aggarwalpulkit596@gmail.com>
2019-11-02 19:02:50 +0530

    Add ImageCapture

--- CameraX/app/src/main/java/com/codingblocks/camerax/MainActivity.kt
+++ CameraX/app/src/main/java/com/codingblocks/camerax/MainActivity.kt
@@ -3,17 +3,20 @@ package com.codingblocks.camerax
 import android.content.pm.PackageManager
 import android.graphics.Matrix
 import android.os.Bundle
+import android.util.Log
 import android.view.Surface
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
-import androidx.camera.core.AspectRatio
-import androidx.camera.core.CameraX
-import androidx.camera.core.Preview
-import androidx.camera.core.PreviewConfig
+import androidx.camera.core.*
 import androidx.core.app.ActivityCompat
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.util.concurrent.Executor
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Executor {
+    override fun execute(p0: Runnable) {
+        p0.run()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +40,42 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun startCamera() {
+        val imageCaptureConfig = ImageCaptureConfig.Builder().apply {
+            setTargetAspectRatio(AspectRatio.RATIO_16_9)
+            setCaptureMode(ImageCapture.CaptureMode.MAX_QUALITY)
+        }.build()
+
+        val imageCapture = ImageCapture(imageCaptureConfig)
+
+        captureImage.setOnClickListener {
+            val file = File(externalMediaDirs.first(), "${System.currentTimeMillis()}.jpg")
+            imageCapture.takePicture(file, this, object : ImageCapture.OnImageSavedListener {
+                override fun onImageSaved(file: File) {
+//                    Toast.makeText(
+//                        this@MainActivity,
+//                        "Image Captured ${file.absolutePath}",
+//                        Toast.LENGTH_LONG
+//                    ).show()
+                    Log.i("IMAGECAPTURE", "Image Captured ${file.absolutePath}")
+                }
+
+                override fun onError(
+                    imageCaptureError: ImageCapture.ImageCaptureError,
+                    message: String,
+                    cause: Throwable?
+                ) {
+//                    Toast.makeText(
+//                        this@MainActivity,
+//                        "Error Capturing $message",
+//                        Toast.LENGTH_LONG
+//                    ).show()
+                    Log.i("IMAGECAPTURE", "Error Capturing $message")
+
+                }
+
+            })
+        }
+
         val previewConfig = PreviewConfig.Builder().apply {
             setTargetAspectRatio(AspectRatio.RATIO_16_9)
             setLensFacing(CameraX.LensFacing.BACK)
@@ -52,7 +91,7 @@ class MainActivity : AppCompatActivity() {
             textureView.surfaceTexture = it.surfaceTexture
         }
 
-        CameraX.bindToLifecycle(this, preview)
+        CameraX.bindToLifecycle(this, preview, imageCapture)
     }
 
     private fun updateTransform() {
@@ -61,7 +100,7 @@ class MainActivity : AppCompatActivity() {
         val centerX = textureView.width / 2f
         val centerY = textureView.height / 2f
 
-        val rotationDegress = when(textureView.display.rotation){
+        val rotationDegress = when (textureView.display.rotation) {
             Surface.ROTATION_0 -> 0
             Surface.ROTATION_90 -> 90
             Surface.ROTATION_180 -> 180
@@ -69,7 +108,7 @@ class MainActivity : AppCompatActivity() {
             else -> return
         }
 
-        matrix.postRotate(-rotationDegress.toFloat(),centerX,centerY)
+        matrix.postRotate(-rotationDegress.toFloat(), centerX, centerY)
 
         textureView.setTransform(matrix)
     }
