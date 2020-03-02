2cb7d0713a287a6ca9248fc58ab5d2b4552fa818
pulkit-mac <aggarwalpulkit596@Gmail.com>
2020-03-02 22:25:36 +0530

    Add Okhttp with GSON

--- NetworkingWithAndroid/app/src/main/java/com/puldroid/networking/MainActivity.kt
+++ NetworkingWithAndroid/app/src/main/java/com/puldroid/networking/MainActivity.kt
@@ -1,30 +1,45 @@
 package com.puldroid.networking
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.gson.FieldNamingPolicy
+import com.google.gson.GsonBuilder
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import okhttp3.OkHttpClient
+import okhttp3.Request
 
 class MainActivity : AppCompatActivity() {
-
-    val adapter = UserAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter.onItemClick = {
-            val intent = Intent(this,UserActivity::class.java)
-            intent.putExtra("ID",it)
-            startActivity(intent)
+        val okHttpClient = OkHttpClient()
+
+
+        val request = Request.Builder()
+            .url("https://api.github.com/users/aggarwalpulkit596")
+            .build()
+
+        val gson = GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+            .create()
+
+
+        GlobalScope.launch(Dispatchers.Main) {
+            val response = withContext(Dispatchers.IO) {
+                okHttpClient.newCall(request).execute().body?.string()
+            }
+            val user = gson.fromJson<User>(response, User::class.java)
+            textView.text = user.name
+            textView2.text = user.login
+            Picasso.get().load(user.avatarUrl).into(imageView)
+
         }
-
-        userRv.apply {
-            layoutManager = LinearLayoutManager(this@MainActivity)
-            adapter = this@MainActivity.adapter
-        }
-
-
     }
+
 }
