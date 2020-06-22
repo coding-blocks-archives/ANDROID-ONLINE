70b1a95171b171725c82f9d6aabbf63fe370ab80
Pulkit Aggarwal <aggarwalpulkit596@gmail.com>
2020-06-22 14:55:02 +0530

    using constraint layout,button and scroll view

--- Layout&Views/app/src/main/java/com/puldroid/layoutviews/MainActivity.kt
+++ Layout&Views/app/src/main/java/com/puldroid/layoutviews/MainActivity.kt
@@ -2,10 +2,16 @@ package com.puldroid.layoutviews
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun showtoast(view: View) {
+        Toast.makeText(this, "This is button click", Toast.LENGTH_SHORT).show()
     }
 }
